9a7d856a42442853508b0570515efd29d11cb4b3
henry.sweat <hi@henrysweat.com>
2024-02-14 16:42:54 -0500

    adjust height for safe area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import Game from '@/components/game';
 
 export default function Home() {
   return (
-    <main className='flex-1 w-full flex flex-col flex-col-reverse items-center relative mx-auto scroll-my-12 overflow-auto px-4 md:pb-4'>
+    <main
+      className={`element flex-1 w-full flex flex-col flex-col-reverse items-center relative mx-auto scroll-my-12 overflow-auto px-4 md:pb-4`}
+    >
       <div className='flex flex-col flex-row-reverse'>
         <Game />
       </div>
