ff2449bf11947636d1113b73cf3f42d74c1c71a6
henry.sweat <hi@henrysweat.com>
2024-02-15 09:46:59 -0500

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       className={`flex-1 w-full flex flex-col flex-col-reverse items-center relative mx-auto scroll-my-12 overflow x-hidden px-4`}
     >
       <div className='flex flex-col flex-row-reverse'>
-        {'test'}
         <Game />
       </div>
     </main>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,7 +3,7 @@ import UserButton from '../user-button';
 
 export default function Header() {
   return (
-    <header className='container relative mx-auto overflow-hidden pb-4 pl-4 pr-4 pt-6 md:pb-4 md:pl-16 md:pr-16 md:pt-12'>
+    <header className='fixed inset-x-0 bottom-0 mx-auto overflow-hidden pb-4 pl-4 pr-4'>
       <div className='flex justify-between mx-auto w-full max-w-2xl bg-white'>
         <nav className='flex space-x-4'>
           <Link
