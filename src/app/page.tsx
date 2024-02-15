3b21758db3d4d90bcb441684c70444447ba7c6c7
henry.sweat <hi@henrysweat.com>
2024-02-15 12:33:03 -0500

    layout testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Game from '@/components/game';
 export default function Home() {
   return (
     <main
-      className={`fixed bottom-0 w-full flex flex-col flex-col-reverse items-center mx-auto scroll-my-12 x-hidden pb-4 px-4`}
+      className={`fixed bottom-0 sm:relative sm:overflow-y-auto sm:h-[600px] w-full flex flex-col flex-col-reverse items-center mx-auto scroll-my-12 x-hidden pb-4 px-4`}
     >
       <Game />
     </main>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import UserButton from '../user-button';
 export default function Header() {
   return (
     <header className='container relative mx-auto overflow-hidden p-6 sm:pt-16'>
-      <div className='flex items-center justify-between mx-auto w-full max-w-lg bg-white'>
+      <div className='flex items-center justify-between mx-auto w-full max-w-lg lg:max-w-2xl bg-white'>
         <nav className='flex space-x-4'>
           <Link href={'/'} className={`hover:underline`}>
             <h1 className='text-lg font-mono text-primary'>Yachtsea</h1>
