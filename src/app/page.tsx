f915cdcc81ad3666d511204ea7e370d47044bef6
henry.sweat <hi@henrysweat.com>
2024-02-15 09:24:10 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       className={`flex-1 w-full flex flex-col flex-col-reverse items-center relative mx-auto scroll-my-12 overflow x-hidden px-4`}
     >
       <div className='flex flex-col flex-row-reverse'>
+        {'test'}
         <Game />
       </div>
     </main>
