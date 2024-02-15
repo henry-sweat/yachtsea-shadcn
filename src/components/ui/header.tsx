778ad8a30dad699d9f927a4c4ea736d0825a94db
henry.sweat <hi@henrysweat.com>
2024-02-15 10:58:20 -0500

    new styling

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -5,15 +5,12 @@ export default function Header() {
   return (
     <header className='container relative mx-auto overflow-hidden p-6'>
       <div className='flex items-center justify-between mx-auto w-full max-w-2xl bg-white'>
-        {/* <nav className='flex space-x-4'>
-          <Link
-            href={'/'}
-            className={`max-w-md text-pretty text-lg text-muted-foreground hover:underline`}
-          >
-            home
+        <nav className='flex space-x-4'>
+          <Link href={'/'} className={`hover:underline`}>
+            <h1 className='text-lg font-mono text-primary'>Yachtsea</h1>
           </Link>
-        </nav> */}
-        <h1 className='text-lg font-mono'>Yachtsea</h1>
+        </nav>
+
         <UserButton />
       </div>
     </header>
