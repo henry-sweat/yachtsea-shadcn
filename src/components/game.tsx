bd9a672f135fc1488a6a9119fa088bb185a60356
henry.sweat <hi@henrysweat.com>
2024-03-04 19:09:51 -0500

    enforce portrait mode

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -3,25 +3,63 @@
 import DiceContainer from './dice/dice-container';
 import Scorecard from './scorecard/scorecard';
 import RollButton from './roll-button/roll-button';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useGameActions } from '@/stores/gameState';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function Game() {
   const { updateUser } = useGameActions();
   const { data: session } = useSession();
+  const [isLandscape, setIsLandscape] = useState(false);
+
+  const checkOrientation = () => {
+    setIsLandscape(window.matchMedia('(orientation: landscape)').matches);
+  };
 
   useEffect(() => {
     updateUser(session);
+
+    checkOrientation();
+    window.addEventListener('resize', checkOrientation);
+
+    return () => window.removeEventListener('resize', checkOrientation);
   }, [session, updateUser]);
 
   return (
-    <div className='flex flex-col-reverse space-y-2 space-y-reverse max-w-screen-sm overflow-auto px-3'>
-      <RollButton />
+    <>
+      {!isLandscape ? (
+        <div
+          id={'game'}
+          className='flex flex-col-reverse space-y-2 space-y-reverse max-w-screen-sm overflow-auto px-3'
+        >
+          <RollButton />
 
-      <DiceContainer />
+          <DiceContainer />
 
-      <Scorecard />
-    </div>
+          <Scorecard />
+        </div>
+      ) : (
+        <OrientationWarning />
+      )}
+    </>
+  );
+}
+
+function OrientationWarning() {
+  return (
+    <Card id='orientation-warning'>
+      <CardHeader>
+        <CardTitle>Hold up!</CardTitle>
+        <CardDescription>
+          This game is enjoyed best in portrait mode. Please adjust your screen.
+        </CardDescription>
+      </CardHeader>
+    </Card>
   );
 }
