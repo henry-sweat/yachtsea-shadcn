87f2eda1160c9c809ccef011ead75bf4ceeb864e
henry.sweat <hi@henrysweat.com>
2024-03-19 12:04:35 -0400

    remove viewport object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,6 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
 };
 
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-};
-
 export default async function RootLayout({
   children,
 }: Readonly<{
