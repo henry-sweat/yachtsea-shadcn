fb40d4c66a05a1a7e52203651c295d49770966fc
henry.sweat <hi@henrysweat.com>
2024-03-19 16:37:27 -0400

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className}`}>
-        <div className='flex flex-col h-full'>
+        <div className='flex flex-col h-dvh'>
           <Header />
           {children}
           <Toaster />
